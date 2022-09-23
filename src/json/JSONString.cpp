1866a2fed184cb36566c62c5d86a5e181152e181
Joshua Binns <joshuabinns.coding@gmail.com>
2022-09-23 18:14:56 +0100

    Added string functionality and fixed Makefile

--- src/json/JSONString.cpp
+++ src/json/JSONString.cpp
@@ -1,1 +1,33 @@
 #include "JSON.h"
+
+JSON::JSONString::JSONString(const char* string) : m_string(string) {
+
+};
+
+JSON::JSONString::JSONString(const std::string& string) : m_string(string) {
+
+};
+
+int JSON::JSONString::length() const {
+    return m_string.size();
+};
+
+bool JSON::JSONString::regex_match(const std::regex& pattern) const {
+    return std::regex_match(m_string, pattern);
+};
+
+bool JSON::JSONString::regex_match(const std::string& pattern) const {
+    return regex_match(std::regex(pattern));
+};
+
+bool JSON::JSONString::regex_match(const char* pattern) const {
+    return regex_match(std::regex(pattern));
+};
+
+char& JSON::JSONString::operator[](int index) {
+    if (index < 0 || index >= m_string.size()) {
+        std::cout << "JSON::JSONString::operator[]: index exceeds bounds of string. Exitting" << std::endl;
+        exit(1);
+    }
+    return m_string[index];
+};
--- src/json/JSONString.h
+++ src/json/JSONString.h
@@ -3,8 +3,16 @@
 
 class JSON::JSONString {
 public:
-    JSONString(const char* string) : m_string(string) { };
-    JSONString(const std::string& string) : m_string(string) { };
+    JSONString(const char* string);
+    JSONString(const std::string& string);
+
+    int length() const;
+
+    bool regex_match(const std::regex& pattern) const;
+    bool regex_match(const std::string& pattern) const;
+    bool regex_match(const char* pattern) const;
+
+    char& operator[](int);
 
 private:
     std::string m_string;

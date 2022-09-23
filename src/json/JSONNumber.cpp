f2e8adda041f648a591b92a3b447bbbebc61e3ba
Joshua Binns <joshuabinns.coding@gmail.com>
2022-09-23 17:16:51 +0100

    Cleaned up number methods

--- src/json/JSONNumber.cpp
+++ src/json/JSONNumber.cpp
@@ -1,2 +1,49 @@
 #include "JSON.h"
 
+JSON::JSONNumber::JSONNumber(const int& number) : m_number(number) {
+
+};
+
+JSON::JSONNumber::JSONNumber(const float& number) : m_number(number) {
+
+};
+
+JSON::JSONNumber::JSONNumber(const double& number) : m_number(number) {
+
+};
+
+JSON::JSONNumber::JSONNumber(const std::string& number) {
+    m_number = atof(number.c_str());
+};
+
+JSON::JSONNumber::JSONNumber(const char* number) {
+    m_number = atof(number);
+};
+
+JSON::JSONNumber JSON::JSONNumber::operator+(const JSON::JSONNumber& number) {
+    return this->m_number + number.m_number;
+};
+
+void JSON::JSONNumber::operator+=(const JSON::JSONNumber& number) {
+    this->m_number += number.m_number;
+};
+
+void JSON::JSONNumber::operator+=(const double& number) {
+    this->m_number += number;
+};
+
+JSON::JSONNumber& JSON::JSONNumber::operator++() {
+    this->m_number += 1.0;
+    return *this;
+};
+
+JSON::JSONNumber JSON::JSONNumber::operator++(int) {
+    JSONNumber temp = *this;
+    ++*this;
+    return temp;
+};
+
+std::ostream& JSON::operator<<(std::ostream& os, const JSON::JSONNumber& number) {
+    os << number.m_number;
+    return os;
+};
--- src/json/JSONNumber.h
+++ src/json/JSONNumber.h
@@ -3,41 +3,21 @@
 
 class JSON::JSONNumber {
 public:
-    JSONNumber(const int& number) : m_number(number) { }
-    JSONNumber(const float& number) : m_number(number) { }
-    JSONNumber(const double& number) : m_number(number) { }
-    JSONNumber(const std::string& number) {
-        m_number = atof(number.c_str());
-    }
-    JSONNumber(const char* number) {
-        m_number = atof(number);
-    }
+    JSONNumber(const int& number);
+    JSONNumber(const float& number);
+    JSONNumber(const double& number);
+    JSONNumber(const std::string& number);
+    JSONNumber(const char* number);
 
-    JSONNumber operator+(const JSONNumber& number) {
-        return this->m_number + number.m_number;
-    }
+    JSONNumber operator+(const JSONNumber& number);
 
-    void operator+=(const JSONNumber& number) {
-        this->m_number += number.m_number;
-    }
-    void operator+=(const double& number) {
-        this->m_number += number;
-    }
+    void operator+=(const JSONNumber& number);
+    void operator+=(const double& number);
 
-    JSONNumber& operator++() {
-        this->m_number += 1.0;
-        return *this;
-    }
-    JSONNumber operator++(int) {
-        JSONNumber temp = *this;
-        ++*this;
-        return temp;
-    }
+    JSONNumber& operator++();
+    JSONNumber operator++(int);
 
-    friend std::ostream& operator<<(std::ostream& os, const JSONNumber& number) {
-        os << number.m_number;
-        return os;
-    }
+    friend std::ostream& operator<<(std::ostream& os, const JSONNumber& number);
 
 private:
     double m_number;
